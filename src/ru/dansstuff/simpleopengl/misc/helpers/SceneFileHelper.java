d35e4f011583790be32eac235412d8bc0789ddad
Dan Kraynov <dan@dansstuff.ru>
2019-03-30 01:27:36 +0300

    Decompose and un-hardcode SceneFileHelper

--- src/ru/dansstuff/simpleopengl/misc/helpers/SceneFileHelper.java
+++ src/ru/dansstuff/simpleopengl/misc/helpers/SceneFileHelper.java
@@ -6,29 +6,32 @@ import com.google.gson.TypeAdapterFactory;
 import com.google.gson.stream.JsonReader;
 import ru.dansstuff.simpleopengl.misc.gson.RuntimeTypeAdapterFactory;
 import ru.dansstuff.simpleopengl.objects.*;
-import ru.dansstuff.simpleopengl.tree.GLNode;
 
 import java.io.*;
 
 public final class SceneFileHelper {
     private static TypeAdapterFactory rFactory = RuntimeTypeAdapterFactory
             .of(GLObject.class, "type")
-            .registerSubtype(Box.class, "Box")
-            .registerSubtype(Triangle.class, "Triangle")
-            .registerSubtype(Sphere.class, "Sphere")
-            .registerSubtype(DirectionalLight.class, "DirectionalLight")
-            .registerSubtype(Cylinder.class, "Cylinder")
-            .registerSubtype(Line.class, "Line");
+            .registerSubtype(Box.class, Box.class.getSimpleName())
+            .registerSubtype(Triangle.class, Triangle.class.getSimpleName())
+            .registerSubtype(Sphere.class, Sphere.class.getSimpleName())
+            .registerSubtype(DirectionalLight.class, DirectionalLight.class.getSimpleName())
+            .registerSubtype(Cylinder.class, Cylinder.class.getSimpleName())
+            .registerSubtype(Line.class, Line.class.getSimpleName());
 
     private static Gson gson = new GsonBuilder().registerTypeAdapterFactory(rFactory).create();
 
-    public static GLNode readScene(File file) throws FileNotFoundException {
+    public static GLObject readScene(File file) throws FileNotFoundException {
         JsonReader reader = new JsonReader(new FileReader(file));
 
-        return gson.fromJson(reader, GLNode.class);
+        return gson.fromJson(reader, GLObject.class);
     }
 
-    public static void writeScene(GLNode root, String filename) throws IOException {
-        new FileWriter(filename).write(gson.toJson(root));
+    public static String getSceneJson(GLObject root) {
+        return gson.toJson(root, GLObject.class);
+    }
+
+    public static void writeScene(GLObject root, String filename) throws IOException {
+        new FileWriter(filename).write(getSceneJson(root));
     }
 }
