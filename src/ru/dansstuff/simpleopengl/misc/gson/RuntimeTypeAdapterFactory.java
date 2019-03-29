e7aec741987c400faac639bff9cfe3fd1336afda
Dan Kraynov <dan@dansstuff.ru>
2019-03-30 01:26:08 +0300

    Add temporary RuntimeTypeAdapterFactory serialization fix

--- src/ru/dansstuff/simpleopengl/misc/gson/RuntimeTypeAdapterFactory.java
+++ src/ru/dansstuff/simpleopengl/misc/gson/RuntimeTypeAdapterFactory.java
@@ -249,10 +249,10 @@ public final class RuntimeTypeAdapterFactory<T> implements TypeAdapterFactory {
 
                 JsonObject clone = new JsonObject();
 
-                if (jsonObject.has(typeFieldName)) {
-                    throw new JsonParseException("cannot serialize " + srcType.getName()
-                            + " because it already defines a field named " + typeFieldName);
-                }
+//                if (jsonObject.has(typeFieldName)) {
+//                    throw new JsonParseException("cannot serialize " + srcType.getName()
+//                            + " because it already defines a field named " + typeFieldName);
+//                }
                 clone.add(typeFieldName, new JsonPrimitive(label));
 
                 for (Map.Entry<String, JsonElement> e : jsonObject.entrySet()) {
